62b5bcb7c9b2b609e5fcd4319e48504ab92bb5d0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-06-16 15:06:14 +0200

    feat: add debuff indicators

---
+++ Assets/Scripts/DebuffIndicator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebuffIndicator : MonoBehaviour
+{
+  [SerializeField] Debuff _debuff;
+  private Image _icon;
+
+  private void Awake()
+  {
+    _icon = GetComponent<Image>();
+  }
+
+  private void Start()
+  {
+    if (_debuff)
+    {
+      _icon.sprite = _debuff.Icon;
+      _icon.enabled = _debuff && _debuff.IsActive;
+    }
+  }
+
+  private void Update()
+  {
+    _icon.enabled = _debuff && _debuff.IsActive;
+  }
+}
